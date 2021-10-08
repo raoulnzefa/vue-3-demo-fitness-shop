eff442e80681537ac743547dae2700cc7c3a921a
tngeene <tngeene27@gmail.com>
2021-10-08 21:39:07 +0300

    setup base vue refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    home
+    <h3>Refs</h3>
+    <p>My name is {{ user.name }} and my age is {{ user.age }}</p>
+    <button @click="updateUser">Update User 1</button>
+    <h3>Reactive</h3>
+    <p>My name is {{ user2.name }} and my age is {{ user2.age }}</p>
+    <button @click="updateUser2">Update User 2</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { ref, reactive } from "@vue/reactivity";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    // const name = ref("Ted");
+    // const age = ref(30);
+    const user = ref({ name: "Lewis", age: 25 });
+    const user2 = reactive({ name: "John", age: 15 });
+    const updateUser = () => {
+      user.value.name = "Lewis";
+      user.value.age = "39";
+    };
+    const updateUser2 = () => {
+      user2.name = "Lawrence";
+      user2.age = "23";
+    };
+    return { user, user2, updateUser, updateUser2 };
   },
 };
 </script>
